30a9b9d8081e6e0c44334dba47f954a25d14c87d
Cullain <lukas.gedrat@gmx.net>
2023-05-11 10:32:41 +0200

    Externe Parameter

--- script_01.js
+++ script_01.js
@@ -3,10 +3,49 @@
 /***** Funktionen 01 *****/
 // 1. Kapselung von Codeblöcken
 
+// Funktionsaufruf (call)
+
+// test();
+
+// Funktionsrumpf (body) | callee
+// Funktionsdeclaration
+
 function test() {
 
     console.log("Hallo wer auch immer");
 
 }
 
-test();
+/***** Funktionen 02a *****/
+// 2a. Parametrisierung + Datenübergabe von INNEN
+
+// ausgabeNamen();
+
+
+
+// function ausgabeNamen() {
+    
+//     console.log("Hallo wer auch immer");
+
+//     let vorName = prompt("Wie heißt du?"); 
+//     // prompt der nur mit entsprechender Anfrage über ausgabeName getriggert wird
+
+//     console.log("Hallo " + vorName);
+
+//         if (vorName != "Lukas") {
+
+//             console.log("Aha");
+
+//         } else {
+//             console.log("Na dann"); 
+//         } 
+// }
+
+ausgabeNamenParam(prompt("Wie heißt du?")) // Argument (Daten für Parameter)
+
+function ausgabeNamenParam(firstName) {
+    
+        console.log("Hallo " + firstName);
+
+}
+
