6fa20b37e5e482226534c34d2b969e1f24e98593
Peter <schmidt.multimedia@gmail.com>
2023-05-11 11:50:32 +0200

    03a. Vorbereitung / Trennung der Funktionalität

--- script_01.js
+++ script_01.js
@@ -40,12 +40,34 @@ function ausgabeNamenParam(firstName) { // Parameter
 }
 
 /***** Funktionen 02c *****/
-// 2c. Mehrere Parameter
+// 2c. Mehrere Parameter / Argumente
 
-ausgabeNamenParams("Dave", "Batista");
-ausgabeNamenParams("Jamshid","Rahmanzani");
-ausgabeNamenParams(prompt("Vorname?"),prompt("Nachname?"));
+// ausgabeNamenParams("Dave", "Batista");
+// ausgabeNamenParams("Jamshid","Rahmanzani");
+// ausgabeNamenParams(prompt("Vorname?"),prompt("Nachname?"));
 
 function ausgabeNamenParams(firstName, familyName) {
     console.log("Hallo " + firstName + " " + familyName + "!"); 
 }
+
+/***** Funktionen 03a *****/
+// 03a. Vorbereitung
+// Postulat: one function = one job (uncle Bob)
+// SRP single responsibility principle
+
+ausgabeNamenParams2("Jamshid","Rahmanzani");
+
+function ausgabeNamenParams2(firstName, familyName) {
+
+    // 1. Funktionalität: string composing
+    const GAP = " ";
+    let outputStr = "Hallo" + GAP + firstName + GAP + familyName + "!";
+
+
+    // 2. Funktionalität: string output
+    console.log(outputStr); 
+
+}
+
+
+
