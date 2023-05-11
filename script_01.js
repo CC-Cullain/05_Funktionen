b066fe97c8c5ca3c257e62f82e13e1acd999d8da
Peter <schmidt.multimedia@gmail.com>
2023-05-11 10:55:57 +0200

    2c. Mehrere Parameter / Argumente

--- script_01.js
+++ script_01.js
@@ -30,12 +30,22 @@ function ausgabeNamen()
 /***** Funktionen 02b *****/
 // 2b. Parametrisierung + Datenübergabe von AUSSEN
 
-ausgabeNamenParam("Asad"); // call + Argument(e)
-ausgabeNamenParam("Roxana");
-ausgabeNamenParam("Sven");
+// ausgabeNamenParam("Asad"); // call + Argument(e)
+// ausgabeNamenParam("Roxana");
+// ausgabeNamenParam("Sven");
 // Argumente sind Daten für Parameter
 
 function ausgabeNamenParam(firstName) { // Parameter
     console.log("Hallo " + firstName + "!"); 
 }
 
+/***** Funktionen 02c *****/
+// 2c. Mehrere Parameter
+
+ausgabeNamenParams("Dave", "Batista");
+ausgabeNamenParams("Jamshid","Rahmanzani");
+ausgabeNamenParams(prompt("Vorname?"),prompt("Nachname?"));
+
+function ausgabeNamenParams(firstName, familyName) {
+    console.log("Hallo " + firstName + " " + familyName + "!"); 
+}
